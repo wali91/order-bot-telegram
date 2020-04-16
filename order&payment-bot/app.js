7117dba98ac266dcfe7090db4df76ee5c260ebee
wali91 <wlrhmn@gmail.com>
2020-04-17 04:12:09 +0700

    revisi juga

--- order&payment-bot/app.js
+++ order&payment-bot/app.js
@@ -56,7 +56,7 @@ if (CONFIG.app === "dev") {
 }
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
